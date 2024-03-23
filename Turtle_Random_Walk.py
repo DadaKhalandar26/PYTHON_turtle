66e263dee1a141c2c63b2740d8b9a14314d693fc
Bandar Shaik Dada Khalandar <123884804+DadaKhalandar26@users.noreply.github.com>
2024-03-23 15:22:52 +0530

    Update Turtle_Random_Walk.py

--- Turtle_Random_Walk.py
+++ Turtle_Random_Walk.py
@@ -15,31 +15,19 @@ def random_colour():
 
 timmy = Turtle()
 timmy.shape("turtle")
-timmy.color("#FF00FF")
 
 colors = ["#0000FF", "#00FFFF", "#EEE8AA", "#FFA500", "#BA55D3",
           "#4B0082", "#008000", "#FFFF00", "#DC143C", "#556B2F"]
+directions = [0, 90, 180, 270]
 
-walks = [timmy.fd(20), timmy.bk(20), timmy.rt(90), timmy.lt(90)]
+timmy.pensize(15)
+timmy.speed("fastest")
 
 
-timmy.pensize(10)
-timmy.speed(5)
-
-walk = True
-while walk:
-    random_number = random.randint(1, 4)
+for _ in range(200):
     timmy.color(random_colour())
-    if random_number == 1:
-        timmy.fd(20)
-    elif random_number == 2:
-        timmy.rt(90)
-        timmy.fd(20)
-    elif random_number == 3:
-        timmy.bk(20)
-    elif random_number == 4:
-        timmy.lt(90)
-        timmy.fd(20)
+    timmy.forward(30)
+    timmy.setheading(random.choice(directions))
 
 
 screen = Screen()
